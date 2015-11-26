1413567a2d1f0377776f50d7728be8a24abf3cf6
chan xuehong <chanxuehong@gmail.com>
2015-11-26 09:41:42 +0800

    init

---
+++ sql/sql.go
@@ -0,0 +1,53 @@
+package sql
+
+import (
+	"database/sql"
+	"sync"
+)
+
+type DB struct {
+	*sql.DB
+
+	stmtSetRWMutex sync.RWMutex
+	stmtSet        map[string]*sql.Stmt // map[query]*sql.Stmt
+}
+
+func NewDB(db *sql.DB) *DB {
+	return &DB{
+		DB:      db,
+		stmtSet: make(map[string]*sql.Stmt),
+	}
+}
+
+func Open(driverName, dataSourceName string) (*DB, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return NewDB(db), nil
+}
+
+// NOTE: 申请的 *sql.Stmt 请不要关闭
+func (db *DB) Prepare(query string) (stmt *sql.Stmt, err error) {
+	db.stmtSetRWMutex.RLock()
+	stmt = db.stmtSet[query]
+	db.stmtSetRWMutex.RUnlock()
+
+	if stmt != nil {
+		return
+	}
+
+	db.stmtSetRWMutex.Lock()
+	defer db.stmtSetRWMutex.Unlock()
+
+	if stmt = db.stmtSet[query]; stmt != nil {
+		return
+	}
+
+	stmt, err = db.DB.Prepare(query)
+	if err != nil {
+		return
+	}
+	db.stmtSet[query] = stmt
+	return
+}
---
+++ sqlx/sqlx.go
@@ -0,0 +1,83 @@
+package sqlx
+
+import (
+	"sync"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type DB struct {
+	*sqlx.DB
+
+	stmtSetRWMutex sync.RWMutex
+	stmtSet        map[string]*sqlx.Stmt // map[query]*sqlx.Stmt
+
+	namedStmtSetRWMutex sync.RWMutex
+	namedStmtSet        map[string]*sqlx.NamedStmt // map[query]*sqlx.NamedStmt
+}
+
+func NewDB(db *sqlx.DB) *DB {
+	return &DB{
+		DB:           db,
+		stmtSet:      make(map[string]*sqlx.Stmt),
+		namedStmtSet: make(map[string]*sqlx.NamedStmt),
+	}
+}
+
+func Open(driverName, dataSourceName string) (*DB, error) {
+	db, err := sqlx.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return NewDB(db), nil
+}
+
+// NOTE: 申请的 *sqlx.Stmt 请不要关闭
+func (db *DB) Preparex(query string) (stmt *sqlx.Stmt, err error) {
+	db.stmtSetRWMutex.RLock()
+	stmt = db.stmtSet[query]
+	db.stmtSetRWMutex.RUnlock()
+
+	if stmt != nil {
+		return
+	}
+
+	db.stmtSetRWMutex.Lock()
+	defer db.stmtSetRWMutex.Unlock()
+
+	if stmt = db.stmtSet[query]; stmt != nil {
+		return
+	}
+
+	stmt, err = db.DB.Preparex(query)
+	if err != nil {
+		return
+	}
+	db.stmtSet[query] = stmt
+	return
+}
+
+// NOTE: 申请的 *sqlx.NamedStmt 请不要关闭
+func (db *DB) PrepareNamed(query string) (stmt *sqlx.NamedStmt, err error) {
+	db.namedStmtSetRWMutex.RLock()
+	stmt = db.namedStmtSet[query]
+	db.namedStmtSetRWMutex.RUnlock()
+
+	if stmt != nil {
+		return
+	}
+
+	db.namedStmtSetRWMutex.Lock()
+	defer db.namedStmtSetRWMutex.Unlock()
+
+	if stmt = db.namedStmtSet[query]; stmt != nil {
+		return
+	}
+
+	stmt, err = db.DB.PrepareNamed(query)
+	if err != nil {
+		return
+	}
+	db.namedStmtSet[query] = stmt
+	return
+}
